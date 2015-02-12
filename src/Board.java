e66ac0fac481ec8294979c102d255ae5b906cd4a
Tommy <tommyproer@gmail.com>
2015-02-12 16:36:52 -0500

    Added in images

--- src/Board.java
+++ src/Board.java
@@ -1,21 +1,46 @@
 import java.awt.*;
-
+import java.util.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 
-public class Board extends JFrame {
+// Prints out the entire board
+public class Board extends JComponent {
 	
-	public Board(){
-		this.setTitle("Chess");
-		this.setSize(1600,845);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
+	private BufferedImage image;
+	
+	public Board() throws Exception{
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// Exit application window close operation
+		window.setBounds(0,0,1600,845);
+		window.getContentPane().add(new Board());
+		window.setVisible(true);				// Lets window be visible
+		
+		/*
+		try{
+			image = ImageIO.read(new File("C:\\Users\\chent10\\Pictures\\cute.png"));
+		}catch (Exception e){
+          e.printStackTrace();
+          System.exit(1);
+        }
+        */
+		
 	}
 	
 	
 	public void paint(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+
+	    Image img1 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\chent10\\Pictures\\cute.png");
+	    g2.drawImage(img1, 10, 10, this);
+	    g2.finalize();
+	    
+		// Make the Board (8x8 board)
+		/*
 		int x = 90;
 		int y = 130;
-		// Make the Board
 		g.setColor(Color.BLACK);
 		for(int i = 0; i < 9; i++){
 			g.drawLine(100, 50 + x*i, 100 + y*8, 50 + x*i);
@@ -23,6 +48,8 @@ public class Board extends JFrame {
 		for(int i = 0; i < 9; i++){
 			g.drawLine(100 + y*i, 50, 100 + y*i, 50 + 8*x);
 		}
+		*/
+		//g.drawImage(image, 40, 40, this);
 
 		
 	}
--- src/Chess.java
+++ src/Chess.java
@@ -1,12 +1,40 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
 
-public class Chess {
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
-	
+public class Chess extends JComponent{
+
+	public void paint(Graphics g) {
+	    Graphics2D g2 = (Graphics2D) g;
+
+	    Image img1 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\chent10\\Pictures\\ChessPieces\\pawn.jpg");
+	    g2.drawImage(img1, 100, 590, this);
+	    
+	    int x = 90;
+		int y = 130;
+		for(int i = 0; i < 9; i++){
+			g2.drawLine(100, 50 + x*i, 100 + y*8, 50 + x*i);
+		}
+		for(int i = 0; i < 9; i++){
+			g2.drawLine(100 + y*i, 50, 100 + y*i, 50 + 8*x);
+		}
+		
+	    g2.finalize();
+	  }
 	
 	
 	public static void main(String[] args) {
+		JFrame window = new JFrame();
+	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    window.setBounds(0, 0, 1800, 1500);
+	    window.getContentPane().add(new Chess());
+	    window.setVisible(true);
+	    
 
-		new Board();
 	}
 
 }
